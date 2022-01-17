30a22d135bbae4834861240d99d1f35cfe3506d5
Rahul <r27m93@gmail.com>
2022-01-17 20:14:55 +0530

    css home

---
+++ src/container/Home.jsx
@@ -0,0 +1,27 @@
+import logo from '../images/logo.svg'
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <header>
+          <div>
+            <img src={logo} alt="logo"></img>
+          </div>
+          <div>
+            <hr />
+          </div>
+          <div>
+            <ul>
+              <ol>00 HOME</ol>
+              <ol>01 DESTINATION</ol>
+              <ol>02 CREW</ol>
+              <ol>03 TECHNOLOGY</ol>
+            </ul>
+          </div>
+        </header>
+        <div></div>
+      </div>
+    </>
+  )
+}
+export default Home
