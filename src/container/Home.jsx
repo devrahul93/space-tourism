7598f3e19ef72b9bce657ec0fc3c30c28836ef96
Rahul <r27m93@gmail.com>
2022-01-19 14:41:57 +0530

    resstting the css

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -19,7 +19,19 @@ const Home = () => {
             </ul>
           </div>
         </header>
-        <div></div>
+        <section>
+          <div>
+            <h4>So, you want to travel to</h4>
+            <h2>Space</h2>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div> Explore</div>
+        </section>
       </div>
     </>
   )
